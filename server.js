379defee54a80484b8bde76c1ba63e522dc35b4f
Stanislav Kuhlevsky <to@stas.tv>
2020-05-23 04:02:19 +0300

    Fix

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static")
 const path = require('path')
 
-app = express()
-//add this middleware
-app.use(history())
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
+const app = express()
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 80
 app.listen(port)
-console.log('server started '+ port) 
+
+console.log('server started '+ port)
