eb00c59e4976268d1fc02bc5885819fc6f7c2ca6
Stanislav Kuhlevsky <to@stas.tv>
2020-05-23 04:04:58 +0300

    Fix

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-
 const express = require('express')
-const history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static")
 const path = require('path')
 
 const app = express()
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
+
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 80
