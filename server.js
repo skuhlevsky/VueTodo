21db80cccce242465c0d7eaca3fe720ab3114369
Stanislav Kuhlevsky <to@stas.tv>
2020-05-23 03:50:10 +0300

    Fix routing

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static")
-const path = require('path')
+// const path = require('path')
 
 const app = express()
 
+const staticFileMiddleware = express.static(__dirname)
+app.use(staticFileMiddleware)
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}))
+app.use(staticFileMiddleware)
 
 const port = process.env.PORT || 80
 app.listen(port)
