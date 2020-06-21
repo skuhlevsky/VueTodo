8495a1d67725ce82c8a71906957c1ea0d7608788
Stanislav Kuhlevsky <to@stas.tv>
2020-06-22 01:23:42 +0300

    remove addNote

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,32 @@
 <template>
   <div>
-    <div 
-        v-for="note of allNotes"
-        :key="note.id"
-        class="note-area">
-        <div class="note-title">
-          <router-link 
-            :to="{ name: 'note', params: { id: note.id } }">
-              {{ note.title }}
-          </router-link>
-        </div>
-       
-        <TodoList 
-          v-if="note.todos.length"
-          :todos="note.todos"
-          :removable="false"
-          :isSelectable="false"
-        />
-        <p v-else>No todos!</p>
+    <div v-for="note of allNotes" :key="note.id" class="note-area">
+      <div class="note-title">
+        <router-link :to="{ name: 'note', params: { id: note.id } }">{{ note.title }}</router-link>
+      </div>
 
+      <TodoList
+        v-if="note.todos.length"
+        :todos="note.todos"
+        :removable="false"
+        :isSelectable="false"
+      />
+      <p v-else>No todos!</p>
     </div>
 
     <button @click="addNote()" class="btn btn__geen">New Note</button>
-
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex"
-import TodoList from '@/components/TodoList'
+import { mapActions, mapGetters } from "vuex";
+import TodoList from "@/components/TodoList";
 export default {
-  computed: mapGetters(['allNotes', 'notesCount']),
+  computed: mapGetters(["allNotes", "notesCount"]),
   methods: {
-    ...mapActions(['addNotes', 'fetchNotes']),
+    ...mapActions(["fetchNotes"]),
     addNote() {
-      this.fetchNotes('New Task')
+      this.fetchNotes("New Task");
     }
   },
   components: {
@@ -42,9 +34,9 @@ export default {
   },
   mounted() {
     if (!this.notesCount) {
-      this.fetchNotes('First task')
-      this.fetchNotes('Second task')
+      this.fetchNotes("First task");
+      this.fetchNotes("Second task");
     }
   }
-}
+};
 </script>
